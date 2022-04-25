bbd2b151378205530afd73d777b0fab08389a4f5
Flavio Madureira <flaviohlm@gmail.com>
2022-04-25 22:19:07 +0100

    Support to mac
    Support to mac os; change in the files structures

---
+++ src/anuncios/FXMLGerenciarAnunciosController.java
@@ -0,0 +1,154 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anuncios;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.channels.FileChannel;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import utils.BingoUtils;
+
+/**
+ * FXML Controller class
+ *
+ * @author Flavio
+ */
+public class FXMLGerenciarAnunciosController implements Initializable {
+
+    @FXML
+    private ScrollPane root;
+    @FXML
+    private TilePane tile;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+        iniciaAnunciantes();
+    }
+
+    @FXML
+    private void addAnuncio() throws IOException {
+        FileChooser.ExtensionFilter imageFilter = new FileChooser.ExtensionFilter("Image Files", "*.jpg", "*.png", "*.jpeg");
+
+        FileChooser fc = new FileChooser();
+        fc.getExtensionFilters().add(imageFilter);
+
+        //File destination = new File(BingoUtils.getAnunciantesPath());
+        
+        List<File> list = fc.showOpenMultipleDialog(new Stage());
+        if (list != null) {
+            for (File file : list) {
+                File destination = new File(BingoUtils.getAnunciantesPath()+"/"+file.getName());
+                copyFile(file, destination);
+            }
+        }
+
+    }
+
+    private void iniciaAnunciantes() {
+
+        File folder = new File(BingoUtils.getAnunciantesPath());
+        File[] listOfFiles = folder.listFiles();
+
+        for (final File file : listOfFiles) {
+            ImageView imageView;
+            imageView = createImageView(file);
+            tile.getChildren().addAll(imageView);
+        }
+
+        root.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER); // Horizontal
+        root.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED); // Vertical scroll bar
+        root.setFitToWidth(true);
+        root.setContent(tile);
+    }
+
+    private ImageView createImageView(final File imageFile) {
+        // DEFAULT_THUMBNAIL_WIDTH is a constant you need to define
+        // The last two arguments are: preserveRatio, and use smooth (slower)
+        // resizing
+
+        ImageView imageView = null;
+        try {
+            final Image image = new Image(new FileInputStream(imageFile), 150, 0, true, true);
+            imageView = new ImageView(image);
+            imageView.setFitWidth(150);
+            imageView.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent mouseEvent) {
+
+                    if (mouseEvent.getButton().equals(MouseButton.PRIMARY)) {
+
+                        //if(mouseEvent.getClickCount() == 2){
+                        System.out.println(imageFile.getName());
+                        try {
+
+                            BorderPane borderPane = new BorderPane();
+                            ImageView imageView = new ImageView();
+                            Image image = new Image(new FileInputStream(imageFile));
+                            imageView.setImage(image);
+                            //imageView.setStyle("-fx-border-color: BLUE");
+                            imageView.setStyle("-fx-background-insets: 0 ;");
+//                                imageView.setFitHeight(stage.getHeight() - 10);
+                            imageView.setPreserveRatio(true);
+                            imageView.setSmooth(true);
+                            imageView.setCache(true);
+                            borderPane.setCenter(imageView);
+                            borderPane.setStyle("-fx-border-color: BLUE");
+
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+
+                        //}
+                    }
+                }
+            });
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return imageView;
+    }
+
+    public static void copyFile(File source, File destination) throws IOException {
+        if(destination.exists()){
+            destination.delete();
+        }
+        FileChannel sourceChannel = null;
+        FileChannel destinationChannel = null;
+        try {
+            sourceChannel = new FileInputStream(source).getChannel();
+            destinationChannel = new FileOutputStream(destination).getChannel();
+            sourceChannel.transferTo(0, sourceChannel.size(), destinationChannel);
+        } finally {
+            if (sourceChannel != null && sourceChannel.isOpen())
+                sourceChannel.close();
+            if (destinationChannel != null && destinationChannel.isOpen())
+                destinationChannel.close();
+       }
+   }
+    
+}
